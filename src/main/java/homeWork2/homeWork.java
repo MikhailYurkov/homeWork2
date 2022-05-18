57fbfd53894d5ebee8b32d99ac21c5388e883222
MikhailYurkov <9906205@gmail.com>
2022-05-19 01:16:41 +0300

    HomeWork 2, 5 task

--- src/main/java/homeWork2/homeWork.java
+++ src/main/java/homeWork2/homeWork.java
@@ -8,17 +8,16 @@ public class homeWork {
 //        from10to20( 1,1 );
 //        positiveOrNegative( -1);
 //        isNegative( -1);
-        stringInQuantity( "GeekBrains",5);
-
+//        stringInQuantity( "GeekBrains",5);
+        leapYear(2000);
         }
 
 
     private static boolean from10to20( int a, int b) {
         a = ThreadLocalRandom.current().nextInt(20);
         b = ThreadLocalRandom.current().nextInt(10);
-        System.out.println(a);
-        System.out.println(b);
-          if (a + b <= 20 && a + b > 10 ) {
+        System.out.println(a +" "+ b);
+            if (a + b <= 20 && a + b > 10 ) {
               System.out.println("true");
               return true;
           } else {
@@ -51,4 +50,15 @@ public class homeWork {
             System.out.println(text);
         }
     }
+
+    private static boolean leapYear(int year) {
+        if ( ((year % 4 == 0) | (!(year % 100 == 0) && (year % 400 == 0)))) {
+            System.out.println(year + " год високосный");
+        return true;
+        }else {
+            System.out.println(year + " год не високосный");
+        return false;
+        }
+    }
 }
+//Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный
